99ed6a5c339617caa93a62c0bb91d2f86a1daa58
Grant <grantmcnally.dev@gmail.com>
2018-04-24 14:25:46 +0100

    computed functions

--- app.js
+++ app.js
@@ -1,4 +1,40 @@
 'use strict';
+new Vue ({
+    el: "#root",
+    data: {
+        newName: '',
+        names:["Peter", "Tony", "Steve"],
+        title: "cool",
+        tmp: false,
+        tasks: [ 
+            { description: "Go for walk", completed: true},
+            { description: "Clean room", completed: false},
+            { description: "Cook dinner", completed: false},
+            { description: "Read book", completed: true},
+            { description: "Check emails", completed: false}
+        ]
+    },
+    computed:{
+      
+        reversedMessage: function() {
+            return this.title.split('').reverse().join('');
+        },
+        incompleteTasks: function() {
+            return this.tasks.filter(task => !task.completed);
+        }
+        
+    },
+    methods: {
+        addName: function() {
+            this.names.push(this.newName);
+            this.newName = '';
+        },
+        toggleVar: function() {
+            this.tmp = true;
+        }
+    }
+}) 
+
 const store = new Vuex.Store({
 	state: {
 		songs: [{
